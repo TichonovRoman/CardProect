8d1f26d530247009cd2b2f14c09e2eba3767ef45
TichonovRoman <romikt@inbox.ru>
2022-04-09 21:11:40 +0300

    better

--- src/ui/components/NavBar/NavBar.tsx
+++ src/ui/components/NavBar/NavBar.tsx
@@ -3,15 +3,27 @@ import {NavLink} from 'react-router-dom';
 import s from "./NavBar.module.css"
 
 const NavBar = () => {
+
+
     return (
         <nav className={s.nav}>
-            <NavLink to="/CardProject/profile" className={s.item}>Profile</NavLink>
-            <NavLink to="/CardProject/login" className={s.item}>Login</NavLink>
-            <NavLink to="/CardProject/newPasswordEnter" className={s.item}>New Password Enter</NavLink>
-            <NavLink to="/CardProject/404" className={s.item}>404</NavLink>
-            <NavLink to="/CardProject/passwordRecovery" className={s.item}>Password Recovery</NavLink>
-            <NavLink to="/CardProject/registration" className={s.item}>Registration</NavLink>
-            <NavLink to="/CardProject/testPage" className={s.item}>Test page</NavLink>
+            <NavLink to="/CardProject/profile"
+                     className={({isActive}) => isActive ? `${s.item} ${s.activeLink}` : `${s.item}`}>Profile</NavLink>
+            <NavLink to="/CardProject/login"
+                     className={({isActive}) => isActive ? `${s.item} ${s.activeLink}` : `${s.item}`}>Login</NavLink>
+            <NavLink to="/CardProject/newPasswordEnter"
+                     className={({isActive}) => isActive ? `${s.item} ${s.activeLink}` : `${s.item}`}>New Password
+                Enter</NavLink>
+            <NavLink to="/CardProject/404"
+                     className={({isActive}) => isActive ? `${s.item} ${s.activeLink}` : `${s.item}`}>404</NavLink>
+            <NavLink to="/CardProject/passwordRecovery"
+                     className={({isActive}) => isActive ? `${s.item} ${s.activeLink}` : `${s.item}`}>Password
+                Recovery</NavLink>
+            <NavLink to="/CardProject/registration"
+                     className={({isActive}) => isActive ? `${s.item} ${s.activeLink}` : `${s.item}`}>Registration</NavLink>
+            <NavLink to="/CardProject/testPage"
+                     className={({isActive}) => isActive ? `${s.item} ${s.activeLink}` : `${s.item}`}>Test
+                page</NavLink>
 
         </nav>
     );
